bd8088d296eb8c133556becec94327bd6ba68332
jpcrs <joaop.carrasco@gmail.com>
2018-03-05 23:45:51 -0300

    prices and random

--- project-stock/src/components/Header.vue
+++ project-stock/src/components/Header.vue
@@ -10,16 +10,16 @@
             <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
             <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
         </ul>
-        <strong class="navbar-text navbar-right">Funds: {{ funds }}</strong>
+        <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">End Day</a></li>
-          <li class="dropdown">
+          <li><a href="#" @click="endDay">End Day</a></li>
+          <li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save & Load
               <span class="caret"></span>
             </a>
             <ul class="dropdown-menu">
-              <li><a href="#">Save Data</a></li>
-              <li><a href="#">Load Data</a></li>
+              <li><a href="#" @click="saveData">Save Data</a></li>
+              <li><a href="#" @click="loadData">Load Data</a></li>
             </ul>
           </li>
         </ul>
@@ -31,10 +31,29 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex';
+
 export default {
+  data() {
+    return {
+      isDropdownOpen: false
+    }
+  },
   computed: {
     funds() {
       return this.$store.getters.funds;
+    }
+  },
+  methods: {
+    ...mapActions(['randomizeStocks']),
+    endDay() {
+      this.randomizeStocks();
+    },
+    saveData() {
+      console.log('saveData');
+    },
+    loadData() {
+      console.log('loadData');
     }
   }
 }
--- project-stock/src/components/portfolio/Stock.vue
+++ project-stock/src/components/portfolio/Stock.vue
@@ -9,10 +9,13 @@
             </div>
             <div class="panel-body">
                 <div class="pull-left">
-                    <input type="number" class="form-control" placeholder="Quantity" v-model.number="quantity">
+                    <input type="number" class="form-control" placeholder="Quantity" v-model.number="quantity" :class="{danger: insufficientQuantity}">
                 </div>
                 <div class="pull-right">
-                    <button class="btn btn-success" @click="sellStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)">Sell</button>
+                    <button class="btn btn-success" @click="sellStock" 
+                    :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)">
+                        {{insufficientQuantity ? 'No Stocks' : 'Sell'}}
+                    </button>
                 </div>
             </div>
         </div>
@@ -26,6 +29,11 @@ export default {
     data() {
         return {
             quantity: 0
+        }
+    },
+    computed: {
+        insufficientQuantity() {
+            return this.quantity > this.stock.quantity;
         }
     },
     methods: {
@@ -44,3 +52,9 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
